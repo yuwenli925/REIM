010bd39cea9835dc3231bb2df6e2772443fc5f56
yuwenli925 <34620862+yuwenli925@users.noreply.github.com>
2024-07-02 20:50:18 +0800

    Add files via upload

--- REIM_time_demo.m
+++ REIM_time_demo.m
@@ -1,56 +1,55 @@
-%For 1/(x^s+d) in [1e-6,1]
-s = 0.5;%s = 1;
-d = sort(rand(100,1)*(1e3-1)+1); 
-ds = d/1e6^s;
-M = 30;
-[~,XT,~,Sg] = REIM(M);
-X = linspace(1e-6,1,5e5)';
-errREIM =zeros(100,1); 
-gx = 1./(repmat(XT,1,length(Sg))+Sg');
-gtest = 1./(repmat(X,1,length(Sg))+Sg');
-for j= 1:length(ds)
-    fx = 1./(XT.^s+ds(j));
-    ftest = 1./(X.^s+ds(j));
-    res = gx\fx;
-    errREIM(j) = max(abs(ftest - gtest*res));
-end
-semilogy(d,errREIM)
+% For 1/(x^s+d) in [1e-6,1]
 
-function [B,XT,Leb,Sg] = REIM(M)
-    a = 1e-6; b = 1;
-    X = unique([a:(0.001-a)/2000:0.001,0.001:(0.01-0.001)/2000:0.01,0.01:(b-0.01)/4000:b]');
-    N = length(X);
-    g = @(b) 1./(X+b);
-    E = 10.^(linspace(-6,2,1000))'; 
-    Q = zeros(N,M); %q
-    Sg = zeros(M,1); %u1,u2...uM
-    Wg = zeros(N,M); %g1,g2...gM
-    T = 0; %tM
-    B = 0; %BijM
-    Sg(1) = E(1); %u1
-    Wg(:,1) = g(Sg(1)); %g1
-    [~,T(1)] = max(abs(Wg(:,1))); 
-    Q(:,1) = Wg(:,1)/Wg(T(1),1); %q1
-    B = Q(T(1),1);
-    Leb(1) = max(abs(Q(:,1)/B));
+s = 0.5;
+
+lambda_min = 1e-6; lambda_max = 1;
+d = sort(rand(100,1)*(1e3-1)+1); 
+Lambda = 1e6;
+
+M = 30;
+[Xm,Bm,Gm] = REIM(M,lambda_min,lambda_max);
+
+Xtest = linspace(lambda_min,lambda_max,5e5)';
+gtest = 1./(Xtest+Bm');
+err = zeros(length(d),1); 
+for i = 1:length(d)
+    phiz = 1./(Xtest.^s+d(i)/Lambda^s);
+    phizi = 1./(Xm.^s+d(i)/Lambda^s);
+    err(i) = norm(phiz - gtest*(Gm\phizi), 'inf');
+end
+semilogy(d,err,'o')
+fprintf('The error is %e\n',max(err));
+
+function [xm,bm,G] = REIM(M,a,b)
+    c = b/1e4; d = b/1e2;
+    xset = unique([a:(c-a)/2000:c,c:(d-c)/2000:d,d:(b-d)/3000:b]');
+    % xset = linspace(a,b,1e4)';
+    N = length(xset);
+    % bset = 10.^(linspace(-3,3,1000))'; 
+    bset = 10.^(linspace(-6,1,1000))';
+    bm = zeros(M,1); %u1,u2...uM
+    xm = zeros(M,1);
+    gm = zeros(N,M); %g1,g2...gM
+    bm(1) = bset(1); %u1
+    gset = 1./(xset+bset');
+    gm(:,1) = gset(:,1);
+    [~,id] = max(abs(gm(:,1))); 
+    xm(1) = xset(id);
+    G = 1/(xm(1)+bm(1));
     for m = 2:M
-        L = zeros(length(E),1);
-        for i = 1:length(E) %argmax res
-            % fi = lsqminnorm(Q(:,1:m-1),g(E(i,:)));
-            fi = Q(:,1:m-1)\g(E(i,:));
-            L(i) = max(abs(g(E(i,:)) - Q(:,1:m-1)*fi));
-            %L(i) = Lp(g(E(i,:)) - Q(:,1:m-1)*fi,p);
+        L = zeros(length(bset),1);
+        for i = 1:length(bset) % argmax res
+            L(i) = norm( gset(:,i) - gm(:,1:m-1)*(G\(1./(xm(1:m-1)+bset(i)))) , 'inf');
         end
-        [~,index] = max(L);
-        Sg(m) = E(index);%um
-        Wg(:,m) = g(Sg(m,:));
-        K = Wg(T,m);
-        Sig = B\K;
-        rM = Wg(:,m) - Q(:,1:(m-1))*Sig;
-        [~,T(m)] = max(abs(rM));
-        Q(:,m) = rM/rM(T(m));
-        B = Q(T,1:m);
-        Leb(m) = norm(Q(:,1:m)/B,"inf");
+        [~,id] = max(L);
+        bm(m) = bset(id); % um
+        gm(:,m) = gset(:,id);
+        rm = gm(:,m) - gm(:,1:m-1)*(G\(1./(xm(1:m-1)+bm(m))));
+        [~,id] = max(abs(rm));
+        xm(m) = xset(id);
+        G = 1./(xm(1:m)+bm(1:m)');
     end
-    XT = X(T);
+    [bm, id] = sort(bm);
+    xm = xm(id);
+    G = G(id,id);
 end
