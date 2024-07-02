a09d265e213f531024bc1776b757045cd234a7cd
yuwenli925 <34620862+yuwenli925@users.noreply.github.com>
2024-07-02 16:51:42 +0800

    Update REIM_power1.m

--- REIM_power1.m
+++ REIM_power1.m
@@ -1,37 +1,35 @@
-%For x^(-s) in [1e-6,1]
-function [B,XT,Leb,Sg] = REIM_power1(M)
-a = 1e-6; b = 1;
-X = unique([a:(0.001-a)/2000:0.001,0.001:(0.01-0.001)/2000:0.01,0.01:(b-0.01)/4000:b]');
-N = length(X);
-g = @(b) 1./(X+b);
-E = 10.^(linspace(-7,1,1000))';
-Q = zeros(N,M); %q
-Sg = zeros(M,1); %-1*poles
-Wg = zeros(N,M); %g1,g2...gM
-T = 0; %tM
-B = 0; %BijM
-Sg(1) = E(1); %u1
-Wg(:,1) = g(Sg(1)); %g1
-[~,T(1)] = max(abs(Wg(:,1))); 
-Q(:,1) = Wg(:,1)/Wg(T(1),1); %q1
-B = Q(T(1),1);
-Leb(1) = max(abs(Q(:,1)/B));%Lebesgue constant
-for m = 2:M
-    L = zeros(length(E),1);
-    for i = 1:length(E) %argmax res
-        fi = Q(:,1:m-1)\g(E(i,:));
-        L(i) = max(abs(g(E(i,:)) - Q(:,1:m-1)*fi));
+% For ratioanl approximation of x^(-s) in [1e-6,1]
+% xm records the interpolation points, bm records the negative pole
+
+function [xm,bm,G] = REIM(M,a,b)
+    xset = unique([a:(0.001-a)/2000:0.001,0.001:(0.01-0.001)/2000:0.01,0.01:(b-0.01)/4000:b]');
+    % xset = linspace(a,b,1e4)';
+    N = length(xset);
+    bset = 10.^(linspace(-7,1,1000))';
+    % bset = 10.^(linspace(0,3,1000))';
+    bm = zeros(M,1); %u1,u2...uM
+    xm = zeros(M,1);
+    gm = zeros(N,M); %g1,g2...gM
+    bm(1) = bset(1); %u1
+    gset = 1./(xset+bset');
+    gm(:,1) = gset(:,1);
+    [~,id] = max(abs(gm(:,1))); 
+    xm(1) = xset(id);
+    G = 1/(xm(1)+bm(1));
+    for m = 2:M
+        L = zeros(length(bset),1);
+        for i = 1:length(bset) % argmax res
+            L(i) = norm( gset(:,i) - gm(:,1:m-1)*(G\(1./(xm(1:m-1)+bset(i)))) , 'inf');
+        end
+        [~,id] = max(L);
+        bm(m) = bset(id); % um
+        gm(:,m) = gset(:,id);
+        rm = gm(:,m) - gm(:,1:m-1)*(G\(1./(xm(1:m-1)+bm(m))));
+        [~,id] = max(abs(rm));
+        xm(m) = xset(id);
+        G = 1./(xm(1:m)+bm(1:m)');
     end
-    [~,index] = max(L);
-    Sg(m) = E(index);%um
-    Wg(:,m) = g(Sg(m,:));
-    K = Wg(T,m);
-    Sig = B\K;
-    rM = Wg(:,m) - Q(:,1:(m-1))*Sig;
-    [~,T(m)] = max(abs(rM));
-    Q(:,m) = rM/rM(T(m));
-    B = Q(T,1:m);
-    Leb(m) = norm(Q(:,1:m)/B,"inf");
-end
-XT = X(T);%interpolation points
+    [bm, id] = sort(bm);
+    xm = xm(id);
+    G = G(id,id);
 end
